d6ee9a85eb2d5d246414b79eab8187784f6a9a4e
Marina Golofaeva <Golofaeva.xs4n@gmail.com+Golofaevap45>
2021-11-07 01:44:26 +0800

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+    appId: "1293290",
+    key: "ff28efbd675796f2529b",
+    secret: "a20becc1a87a9f48feb3",
+    cluster: "eu",
+    useTLS: true,
+});
+// import { WebSocketServer } from 'ws';
+
+// const wss = new WebSocketServer({ port: 3000 });
+
+// wss.on('connection', function connection(ws) {
+//   console.log('New user has been connected...');
+//   ws.send('Welcome new user');
+//   ws.on('message', function incoming(message) {
+//     console.log('received: %s', message);
+//     ws.send('We got your message....');
+//   });
+
+// });
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    const { clickId } = req.query;
+    pusher.trigger("my-channel-1", "my-event", {
+        text: "hello world",
+        content: "money",
+        clickId: clickId,
+    });
+    res.status(200).json({ name: "John Doe" });
 }
---
+++ pages/index.jsx
@@ -0,0 +1,85 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Pusher from "pusher-js";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+    const router = useRouter();
+    const [content, setContent] = useState("safe");
+    useEffect(() => {
+        if (router.query.clickId) {
+            localStorage.setItem("clickId", router.query.clickId);
+        }
+        console.log("clickId =", router.query);
+    }, [router.query.clickId]);
+
+    useEffect(() => {
+        Pusher.logToConsole = true;
+
+        var pusher = new Pusher("ff28efbd675796f2529b", {
+            cluster: "eu",
+        });
+
+        var channel = pusher.subscribe("my-channel-1");
+        channel.bind("my-event", function (data) {
+            const clickId = localStorage.getItem("clickId");
+            console.log("clickId == data.clickId", clickId, data.clickId);
+
+            if (clickId === data.clickId) {
+                setContent(data.content);
+            }
+        });
+    }, []);
+
+    if (content == "safe")
+        return (
+            <div className={styles.container}>
+                <Head></Head>
+
+                <main className={styles.main}>
+                    <div>"Safe content"</div>
+                    <div>{/* <button onClick={() => {}}>AddNewMessage</button> */}</div>
+                </main>
+
+                <footer className={styles.footer}>
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Powered by{" "}
+                        <span className={styles.logo}>
+                            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                        </span>
+                    </a>
+                </footer>
+            </div>
+        );
+    else {
+        return (
+            <div className={styles.container}>
+                <Head></Head>
+
+                <main className={styles.main}>
+                    <h1>"BLACK CONTENT"</h1>
+                    <div>{/* <button onClick={() => {}}>AddNewMessage</button> */}</div>
+                </main>
+
+                <footer className={styles.footer}>
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Powered by{" "}
+                        <span className={styles.logo}>
+                            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                        </span>
+                    </a>
+                </footer>
+            </div>
+        );
+    }
+}
